b12091a0457bd5810709fb30e7106a2590163172
iudizm <iudi@lepidus.com.br>
2024-04-18 18:45:33 -0300

    Update shariff version in path
    Signed-off-by: iudizm <iudi@lepidus.com.br>
    Signed-off-by: yves <yves@lepidus.com.br>

--- ShariffPlugin.inc.php
+++ ShariffPlugin.inc.php
@@ -218,8 +218,8 @@ class ShariffPlugin extends GenericPlugin {
 			$requestedUrl = $doiUrl ?: $request->getCompleteUrl();
 			$request = new PKPRequest();
 			$baseUrl = $request->getBaseUrl();
-			$jsUrl = $baseUrl .'/'. $this->getPluginPath().'/shariff-3.2.1/shariff.complete.js';
-			$shariffCssUrl = $baseUrl .'/' . $this->getPluginPath() . '/shariff-3.2.1/shariff.complete.css';
+			$jsUrl = $baseUrl .'/'. $this->getPluginPath().'/shariff-3.3.0/shariff.complete.js';
+			$shariffCssUrl = $baseUrl .'/' . $this->getPluginPath() . '/shariff-3.3.0/shariff.complete.css';
 			$cssUrl = $baseUrl .'/' . $this->getPluginPath() . '/css/shariff.css';
 			$backendUrl = $baseUrl .'/'. 'shariff-backend';
 			if ($context->getData('shariffEnableWCAG')===NULL?true:(bool)$context->getData('shariffEnableWCAG')) {
